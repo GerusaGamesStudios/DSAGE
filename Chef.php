89ea13a13502367b76050d36140e19e6bb51cce5
GerusaGamesStudios <gerar_99@hotmail.es>
2020-02-07 10:52:37 -0600

    nuevas clases

---
+++ Chef.php
@@ -0,0 +1,5 @@
+<?php
+    class Chef extends Personal{
+        
+    }
+?>
---
+++ Personal.php
@@ -0,0 +1,11 @@
+<?php
+    class Personal{
+        private $nombre;
+        private $apellido;
+        function __construct($nom,$apell)
+        {
+            $this->nombre = $nom;
+            $this->apellido = $apell;
+        }
+    }      
+?>
