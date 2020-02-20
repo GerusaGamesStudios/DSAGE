ebc05aaace34a003d66bd6180b36f80d965ddc7d
GerusaGamesStudios <gerar_99@hotmail.es>
2020-02-20 09:53:18 -0600

    estilos

---
+++ View/pages/index.view.php
@@ -0,0 +1,3 @@
+<main>
+    Pagina principal
+</main>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require 'View/headers/header.view.php';
+require 'View/pages/index.view.php';
+require 'View/footers/footer.view.php'
+?>
