3333448cde1bf8a7bd05d94432f0cf30e3555093
GerusaGamesStudios <gerar_99@hotmail.es>
2020-02-20 14:30:56 -0600

    estilos nuevos

--- View/headers/header.view.php
+++ View/headers/header.view.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../estilos/header.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="View/estilos/header.css">
+    <link href="https://fonts.googleapis.com/css?family=Courgette&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Cardo|Courgette&display=swap" rel="stylesheet"> 
+    <title>Mayahuel</title>
 </head>
 <body>
     
--- View/pages/index.view.php
+++ View/pages/index.view.php
@@ -1,3 +1,21 @@
+<Header>
+    <div  class="title">
+        <h2>Mayahuel</h2>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="">Menú</a></li>
+            <li><a href="">Reservación</a></li>
+            <li><a href="">Ingreso</a></li>
+        </ul>
+    </nav>
+</Header>
 <main>
-    Pagina principal
+    <section class="screen bg1">
+        <div class="secCont">
+            <div class="title">
+                <h2 class="wt">Mayahuel</h2>
+            </div>
+        </div>
+    </section>
 </main>
