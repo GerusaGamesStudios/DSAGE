3702202b1b049977dd63874fe0e87aac251e1f73
LuisSanHer <luish_991@hotmail.com>
2020-03-03 10:03:54 -0600

    Index

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-/*require 'View/headers/header.index.php';
+require 'View/headers/header.index.php';
 require 'View/pages/index.view.php';
-require 'View/footers/footer.view.php'*/
+require 'View/footers/footer.view.php'
 
-require 'Model/Cuenta.php';
+/*require 'Model/Cuenta.php';
 echo("Hola mundo");
 $c = new Cuenta('12345678','Gerente','Luish_991@hotmail.com','Galaxyj7', 'Galaxyj7');
-echo $c->VisualizarCuenta();
+echo $c->VisualizarCuenta();*/
 ?>
