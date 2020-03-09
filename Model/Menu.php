40e911599be30c2c1fd945ed571a18f64e6081c6
GerusaGamesStudios <gerar_99@hotmail.es>
2020-03-09 10:50:24 -0600

    funcion Menu

--- Model/Menu.php
+++ Model/Menu.php
@@ -1,5 +1,18 @@
 <?php
+    include "../Services/BaseDeDatos.php";
     class Menu{
-        
+        private $db;
+        function __construct()
+        {
+            $db = new BaseDatos('localhost:3307','maya','utf-8','root','');
+            
+        }
+        function getAllProductosBd(){
+            $conexion  = $this->db->getConexion();
+            $stat = $conexion->prepare('SELECT * FROM cuentas');
+            $stat->execute();
+            $result = $stat->fetchAll();
+            return $result;
+        }
     }
 ?>
