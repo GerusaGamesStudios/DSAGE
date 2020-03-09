542d9049824f40d00df3ee312dbafb387d6c0fd7
GerusaGamesStudios <gerar_99@hotmail.es>
2020-03-09 11:59:34 -0600

    productos mostrandose en la pagina de productos

--- View/pages/menu.view.php
+++ View/pages/menu.view.php
@@ -1,10 +1,22 @@
+<?php
+include (dirname(__DIR__)."../../Model/Menu.php");
+$m = new Menu();
+$productos = $m->getAllProductosBd();
+?>
 <div class="headSpace">
 
 </div>
 <section>
     <div class="secCont">
         <div class="menuPrincipal">
-
+        <?php foreach($productos as $producto):?>
+            <div class="producto">
+                <h1><?php echo $producto["NOMBRE"]?></h1>
+                <h2> <?php echo $producto["PRECIO"]?>  MXN </h2>
+                <h3><?php echo $producto["DESCRIPCION"]?></h3>
+                <h3><?php echo $producto["TIPO"]?></h3>
+            </div>
+        <?php endforeach?>
         </div>
     </div>
 </section>
