5eed385d7f5f234d40f07291461bee7c869b522f
GerusaGamesStudios <gerar_99@hotmail.es>
2020-03-09 11:43:10 -0600

    menu funcional

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
-// require 'View/headers/header.index.php';
-// require 'View/pages/index.view.php';
-// require 'View/footers/footer.view.php';
-require 'Model/Menu.php';
-
-$m = new Menu();
-$datos = $m->getAllProductosBd();
-var_dump( $datos);
-
+require 'View/headers/header.index.php';
+require 'View/pages/index.view.php';
+require 'View/footers/footer.view.php';
 ?>
