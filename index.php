4d7870091a84fc44bde27f5732710d1cfe6bdd91
LuisSanHer <luish_991@hotmail.com>
2020-03-09 11:20:56 -0600

    CometarioCerrado

--- index.php
+++ index.php
@@ -6,5 +6,5 @@ require 'View/footers/footer.view.php'
 /*require 'Model/Cuenta.php';
 echo("Hola mundo");
 $c = new Cuenta('12345678','Gerente','Luish_991@hotmail.com','Galaxyj7', 'Galaxyj7');
-echo $c->VisualizarCuenta();
+echo $c->VisualizarCuenta();*/
 ?>
