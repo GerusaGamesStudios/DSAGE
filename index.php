84f021f248dfc4c30699898a50ebb2a1c29d2f94
LuisSanHer <luish_991@hotmail.com>
2020-03-10 09:47:58 -0600

    Parametros Cuenta DB

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-require 'View/headers/header.index.php';
+/*require 'View/headers/header.index.php';
 require 'View/pages/index.view.php';
-require 'View/footers/footer.view.php';
+require 'View/footers/footer.view.php';*/
+
+require 'Model/Cuenta.php';
+$cuenta = new Cuenta();
+$array = ;
+$cuenta->RegistrarCuenta($array);
 ?>
