ec3361fceeb8735c85fa2596a0f8be4170a809fd
LuisSanHer <luish_991@hotmail.com>
2020-03-11 10:08:11 -0600

    Add/Delete Producto

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-     require 'View/headers/header.index.php';
-     require 'View/pages/index.view.php';
-     require 'View/footers/footer.view.php';
+      require 'View/headers/header.index.php';
+      require 'View/pages/index.view.php';
+      require 'View/footers/footer.view.php';
 
     //Prueba para Añadir cuenta a la DB;;;
-    //   require 'Model/Cuenta.php';
-    //   $cuenta = new Cuenta('9876', 'Mesero', 'luish_991@hotmail.com', 'wordless', 'wordless');
-    //   $cuenta->VisualizarCuenta();
-    //   $cuenta->AñadirCuenta();
+    //    require 'Model/Producto.php';
+    //    $p = new Producto('1234', 'Nombre', '123', 'Descripcion', '4','Comida');
+    //    $p->VisualizarProducto();
+    //    $p->EliminarProducto();
 ?>
