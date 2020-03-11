8038072069899c1599b3f2d8e5db3204676852a2
LuisSanHer <luish_991@hotmail.com>
2020-03-11 13:26:56 -0600

    kjhk

--- Model/GestorOrden.php
+++ Model/GestorOrden.php
@@ -4,7 +4,19 @@
         private $ArrayOrdenes = array();
         function __construct()
         {
-            
+            try {
+                $conexion  = $this->db->getConexion();
+                $stat = $conexion->prepare('SELECT * FROM ordenes');
+                $stat->execute();
+                $result = $stat->fetchAll();
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+            //Pasando cada registro al array de ordenes
+            while ($reg=mysqli_fetch_array($result))
+            {
+              array_push($this->ArrayOrdenes, $reg);
+            }
         }
 
         function getOrdenes(){
