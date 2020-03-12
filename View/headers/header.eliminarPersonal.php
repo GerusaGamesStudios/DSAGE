bfa06355d54a7e1066c9ab09728bfffa4e51fa14
LuisSanHer <57678190+LuisSanHer@users.noreply.github.com>
2020-03-11 23:53:52 -0600

    BOOTSTRAP

--- View/headers/header.eliminarPersonal.php
+++ View/headers/header.eliminarPersonal.php
@@ -8,6 +8,10 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
+    <!--BOOTSTRAP-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
     <title>Eliminar Personal</title>
 </head>    
-</html>
