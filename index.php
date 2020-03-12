5b8b64514495a12eb1add90a2e6b6d395dfed817
LuisSanHer <57678190+LuisSanHer@users.noreply.github.com>
2020-03-12 00:58:28 -0600

    Index Normal

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-    //require 'View/headers/header.index.php';
-    //require 'View/pages/index.view.php';
-    //require 'View/footers/footer.view.php';
-
-    require 'View/headers/header.eliminarPersonal.php';
-    require 'View/pages/eliminarPersonal.view.php';
+    require 'View/headers/header.index.php';
+    require 'View/pages/index.view.php';
     require 'View/footers/footer.view.php';
+
+    //require 'View/headers/header.eliminarPersonal.php';
+    //require 'View/pages/eliminarPersonal.view.php';
+    //require 'View/footers/footer.view.php';
 ?>
