16eba960d649445b85d799f85c79c9d04aa9f9c9
LuisSanHer <57678190+LuisSanHer@users.noreply.github.com>
2020-10-07 12:10:58 -0500

    nuevo link

--- Services/BaseDeDatos.php
+++ Services/BaseDeDatos.php
@@ -3,7 +3,7 @@
         private $pdobject;
         function __construct($host,$dbname,$charset,$user,$password)
         {
-            $puerto = ':3306';
+            $puerto = ':3307';
 
             try {
                 $this->pdobject = new PDO('mysql:host='.$host. $puerto .';dbname='.$dbname.';charset='.$charset,$user,$password); 
